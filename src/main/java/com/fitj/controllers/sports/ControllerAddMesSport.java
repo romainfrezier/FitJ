7412b81d1eb5b68eafbe3084def393e554dc8218
pacom <paco.munarriz@gmail.com>
2022-12-27 02:02:47 +0100

    Commencement de la partie information

---
+++ src/main/java/com/fitj/controllers/sports/ControllerAddMesSport.java
@@ -0,0 +1,45 @@
+package com.fitj.controllers.sports;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+/**
+ * Controller de la page d'ajout d'un sport
+ * @see ControllerSport
+ * @author Romain Frezier
+ */
+public class ControllerAddMesSport extends ControllerSport {
+
+    // Composants FXML -----------------------------------------------------------------------------------------------
+    @FXML
+    private TextField sportName;
+    @FXML
+    private Button addSportButton;
+    @FXML
+    private Text errorText;
+    // ---------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Méthode appelée lors du chargement de la page
+     */
+    @FXML
+    private void initialize() {
+        super.hideError(errorText);
+    }
+
+    /**
+     * Méthode appelée lors du clic sur le bouton "Ajouter". Ajoute le sport
+     */
+    @FXML
+    private void addSport() {
+        try {
+            hideError(errorText);
+            sportFacade.createSport(sportName.getText());
+            super.goToMonEspace(addSportButton);
+        } catch (Exception e) {
+            super.displayError(errorText, e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/fitj/controllers/sports/ControllerMesSportList.java
@@ -0,0 +1,149 @@
+package com.fitj.controllers.sports;
+
+import com.fitj.classes.Sport;
+import com.fitj.exceptions.BadPageException;
+import com.fitj.exceptions.UnselectedItemException;
+import com.fitj.facades.FacadeSport;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+import javafx.util.Callback;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Controller de la page sport-list-view.fxml
+ * @see ControllerSport
+ * @author Romain Frezier
+ */
+public class ControllerMesSportList extends ControllerSport {
+
+    // Composants FXML -----------------------------------------------------------------------------------------------
+    @FXML
+    private Button addSportButton;
+    @FXML
+    private ListView<Sport> listView;
+    @FXML
+    private Button updateSportButton;
+    @FXML
+    private Button deleteSportbutton;
+    @FXML
+    private Text errorText;
+    // ---------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Méthode appelée lors du chargement de la page
+     */
+    @FXML
+    private void initialize() {
+        super.hideError(errorText);
+        initializeSportList();
+    }
+
+    /**
+     * Methode permettant d'initialiser la liste des sports
+     */
+    private void initializeSportList() {
+        try {
+            List<Sport> sports = sportFacade.getAllSports();
+            listView.setCellFactory(new Callback<>() {
+                @Override
+                public ListCell<Sport> call(ListView<Sport> param) {
+                    return new ListCell<>() {
+                        @Override
+                        protected void updateItem(Sport item, boolean empty) {
+                            super.updateItem(item, empty);
+                            if (item != null) {
+                                setText(item.getNom());
+                            } else {
+                                setText("");
+                            }
+                        }
+                    };
+                }
+            });
+            for (Sport sport : sports) {
+                listView.getItems().add(sport);
+            }
+        } catch (Exception e) {
+            super.displayError(errorText, e.getMessage());
+        }
+    }
+
+    /**
+     * Methode permettant de se rendre sur la page d'ajout d'un sport
+     */
+    @FXML
+    private void goToAddSport() {
+        try {
+            super.hideError(errorText);
+            super.goToAddSport(addSportButton);
+        } catch (BadPageException e) {
+            super.displayError(errorText, e.getMessage());
+        }
+    }
+
+    /**
+     * Methode permettant de se rendre sur la page de modification d'un sport
+     */
+    @FXML
+    private void goToUpdateSport() {
+        try {
+            super.hideError(errorText);
+            checkSelected();
+            setIdObjectSelected(listView.getSelectionModel().getSelectedItem().getId());
+            super.goToUpdateSport(updateSportButton);
+        } catch (BadPageException | UnselectedItemException e) {
+            super.displayError(errorText, e.getMessage());
+        }
+    }
+
+    /**
+     * Methode permettant de supprimer un sport
+     */
+    @FXML
+    private void deleteSport() {
+        try {
+            hideError(errorText);
+            checkSelected();
+            setIdObjectSelected(listView.getSelectionModel().getSelectedItem().getId());
+            showConfirmationDeleteSport();
+        } catch (UnselectedItemException e) {
+            super.displayError(errorText, e.getMessage());
+        }
+    }
+
+    /**
+     * Methode permettant de verifier si un sport est selectionné
+     * @throws UnselectedItemException si aucun sport n'est selectionné
+     */
+    private void checkSelected() throws UnselectedItemException {
+        if (listView.getSelectionModel().getSelectedItem() == null) {
+            throw new UnselectedItemException("Vous devez selectionner un sport");
+        }
+    }
+
+    /**
+     * Méthode permettant d'afficher une fenêtre de confirmation de suppression d'un sport
+     */
+    private void showConfirmationDeleteSport() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Delete Sport");
+        alert.setHeaderText("Vous êtes sûr de vouloir supprimer ce sport ?");
+        alert.setContentText("Vous ne pourrez pas revenir en arrière");
+
+        Optional<ButtonType> option = alert.showAndWait();
+
+        if (option.isPresent() && option.get() == ButtonType.OK){
+            FacadeSport facadeSport = FacadeSport.getInstance();
+            try {
+                facadeSport.deleteSport(getIdObjectSelected());
+                listView.getItems().remove(listView.getSelectionModel().getSelectedItem());
+            } catch (Exception e) {
+                super.displayError(errorText, e.getMessage());
+            }
+        }
+
+    }
+}
